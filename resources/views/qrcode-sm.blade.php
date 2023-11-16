eda9d5ef330d865fe7fc9a59ca8747e5da454f95
rizky14045 <mrizkysaputra17@gmail.com>
2023-11-16 09:39:31 +0700

    change qrcod 2e

--- resources/views/qrcode-sm.blade.php
+++ resources/views/qrcode-sm.blade.php
@@ -7,11 +7,11 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('vendor/lakers') }}/img/new-logo.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        .pjb-qrcode image {
+        .pjb-qrcode img {
                 width: 30%;
             }
         @media only screen and (max-width: 600px) {
-            .pjb-qrcode image {
+            .pjb-qrcode img {
                 width: 70%;
             }
         }
